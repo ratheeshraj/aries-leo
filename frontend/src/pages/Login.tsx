359553f0ec1ad300b1284eb09491a5d0be5c9dae
itsarvindrajan <88017811+itsarvindrajan@users.noreply.github.com>
2025-08-17 05:04:16 +0000

    refactor: remove unused import from Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,7 +9,6 @@ import {
   LockClosedIcon,
   ShieldCheckIcon,
   ArrowLeftIcon,
-  ArrowRightIcon,
 } from "@heroicons/react/24/outline";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import Button from "../components/ui/Button";
